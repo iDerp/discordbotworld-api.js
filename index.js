1c42927e0165172f740d4b41f3b10b4644e35dbe
iDerp <nec17444@hotmail.com>
2018-03-27 16:24:07 -0400

    hmm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const snek = require('snekfetch');
 const apiEndpoint = 'https://discordbot.world/api';
 
-exports.printMsg = function () {
-    console.log("This is a message from the demo package");
-}
+module.exports = {
+    printMsg: console.log("This is a message from the demo package")
+};
